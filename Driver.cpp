bdcd6075bb21e90fc451dbcf62b200d7c3d954bf
eightfold <97198162+8ightfold@users.noreply.github.com>
2024-09-20 21:20:48 -0400

    testing

--- Driver.cpp
+++ Driver.cpp
@@ -1,42 +1,22 @@
 #include <cstddef>
 #include <cstdint>
-
-extern "C" {
-#include <EXIParser.h>
-}
+#include <exip/EXIParser.h>
 
 namespace exi {
+  using CParser = ::Parser;
   using CBinaryBuffer = ::BinaryBuffer;
-
-  struct IBinaryBuffer {
-    struct AlignAndSize {
-      std::size_t size;
-      char data[1];
-    };
-
-  public:
-    CBinaryBuffer getCBuffer();
-  };
-
-  template <std::size_t N>
-  struct BinaryBuffer : IBinaryBuffer {
-    std::size_t size = N;
-    char data[N];
-  };
-
-  inline CBinaryBuffer IBinaryBuffer::getCBuffer() {
-    using CastType = IBinaryBuffer::AlignAndSize*;
-    auto* proxy_ptr = reinterpret_cast<CastType>(this);
-    return { proxy_ptr->data, proxy_ptr->size, 0 };
-  }
-
-  static_assert(
-    sizeof(IBinaryBuffer::AlignAndSize) == sizeof(BinaryBuffer<1>)
-  );
 }
+
+struct AppData {
+  unsigned elementCount;
+  unsigned nestingLevel;
+};
 
 int main() {
   Parser parser;
-  exi::BinaryBuffer<512> buffer;
-  initParser(&parser, buffer.getCBuffer(), nullptr);
+  // exi::BinaryBuffer<512> buffer;
+  AppData data {};
+
+  // initParser(&parser, buffer.getCBuffer(), &data);
+  // destroyParser(&parser);
 }
