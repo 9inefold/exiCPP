e4392a75635a59d494415c1cca794bb70c40bfd1
eightfold <97198162+8ightfold@users.noreply.github.com>
2024-09-30 16:22:46 -0400

    add rapidxml

--- Driver.cpp
+++ Driver.cpp
@@ -19,6 +19,8 @@
 #include <exicpp/Content.hpp>
 #include <exicpp/BinaryBuffer.hpp>
 #include <exip/EXIParser.h>
+#include <exip/EXISerializer.h>
+#include <rapidxml.hpp>
 
 namespace exi {
 
@@ -68,7 +70,35 @@ private:
 
 /////////////////////////////////////////////////////////////////////////
 
+#include <filesystem>
+#include <fstream>
 #include <iostream>
+#include <iterator>
+#include <string>
+
+namespace fs = std::filesystem;
+using Str = std::string;
+using BoxedStr = std::unique_ptr<Str>;
+
+BoxedStr read_file(fs::path filepath) {
+  if(filepath.is_relative()) {
+    filepath = (fs::current_path() / filepath);
+  }
+  std::ifstream is ( filepath, std::ios::binary );
+  is.unsetf(std::ios::skipws);
+  if(!is) return BoxedStr();
+
+  std::streampos size;
+  is.seekg(0, std::ios::end);
+  size = is.tellg();
+  is.seekg(0, std::ios::beg);
+
+  Str file_data;
+  std::istream_iterator<char> start(is), end;
+  file_data.reserve(size);
+  file_data.insert(file_data.cbegin(), start, end);
+  return std::make_unique<Str>(std::move(file_data));
+}
 
 struct AppData {
   unsigned elementCount;
@@ -89,15 +119,55 @@ struct Example {
   }
 };
 
+void iter_nodes(rapidxml::xml_node<>* pnode, std::size_t depth = 0) {
+  std::string padding(depth, ' ');
+  for (auto* node = pnode; node; node = node->next_sibling()) {
+    if (node->type() == rapidxml::node_data) {
+      exi::StrRef val(node->value(), node->value_size());
+      std::cout << padding << '[' << val << "]\n";
+      continue;
+    }
+
+    exi::StrRef name(node->name(), node->name_size());
+    std::cout << padding << name << ":\n";
+
+    for (auto* attr = node->first_attribute();
+      attr; attr = attr->next_attribute())
+    {
+      exi::StrRef attr_name(attr->name(), attr->name_size());
+      exi::StrRef attr_val(attr->value(), attr->value_size());
+      std::cout << padding << " {" << name
+        << attr_name << '=' << attr->value() << "}\n";
+    }
+
+    auto* first_node = node->first_node();
+    if (first_node) {
+      iter_nodes(first_node, depth + 2);
+    }
+  }
+}
+
 int main() {
   exi::Char stackData[512] {};
   exi::BinaryBuffer buf(stackData);
   AppData appData {};
   auto parser = exi::Parser::New<Example>(buf, &appData);
-
-  auto err = parser.parseHeader();
+  // auto err = parser.parseHeader();
   // std::cout << exi::getErrString(err) << std::endl;
 
   // initParser(&parser, buffer.getCBuffer(), &data);
   // destroyParser(&parser);
+
+  auto file = read_file("examples/Customers.xml");
+  if (!file) {
+    std::cerr << "Unable to locate file!\n";
+    return 1;
+  }
+
+  using namespace rapidxml;
+  xml_document<exi::Char> doc;
+  doc.parse<0>(file->data());
+
+  auto* first_node = doc.first_node();
+  iter_nodes(first_node);
 }
