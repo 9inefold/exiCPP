1179ed23cf27a7af3054a6bb6efb9ed15edafbcb
eightfold <97198162+8ightfold@users.noreply.github.com>
2024-10-02 01:47:10 -0400

    Add reader/writer

--- include/exicpp/Reader.hpp
+++ include/exicpp/Reader.hpp
@@ -22,8 +22,104 @@
 #define EXIP_READER_HPP
 
 #include "Basic.hpp"
+#include "BinaryBuffer.hpp"
+#include "Content.hpp"
+#include <exip/EXIParser.h>
+// #include <exip/grammarGenerator.h>
 
 namespace exi {
+
+using CParser = exip::Parser;
+
+class Parser : protected CParser {
+  Parser() noexcept : CParser() {}
+  void(*shutdown)(CParser*) = nullptr;
+public:
+  ~Parser() { this->deinit(); }
+public:
+  template <class Source>
+  [[nodiscard]]
+  static Parser New(Source& appData, const StackBuffer& buf) {
+    Parser parser {};
+    if (Error E = parser.init(&buf, &appData)) {
+#if EXICPP_DEBUG && 0
+      std::printf("\u001b[31;1m");
+      std::printf("Error initializing: %s\n", E.message().data());
+      std::printf("\u001b[0m");
+      std::fflush(stdout);
+#endif
+    }
+    ContentHandler::SetContent(parser.handler, &appData);
+    return parser;
+  }
+
+public:
+  /// @brief Parses the EXI header.
+  /// @return An `Error` on failure.
+  [[nodiscard]] Error parseHeader(bool outOfBandOpts = false) {
+    /*
+    const CErrCode ret = exip::parseHeader(
+      this, exip::boolean(outOfBandOpts));
+    */
+    const CErrCode ret = exip::parseHeader(
+      this, exip::boolean(outOfBandOpts));
+    // Fill with the default schema.
+    (void) this->setSchema(nullptr);
+    return Error::From(ErrCode(ret));
+  }
+
+  /// @brief Sets the schema.
+  /// @return An `Error` on failure.
+  [[nodiscard]] Error setSchema(exip::EXIPSchema* schema = nullptr) {
+    const CErrCode ret = exip::setSchema(this, schema);
+    return Error::From(ErrCode(ret));
+  }
+
+  /// @brief Parses the next item.
+  /// @return `ErrCode::Ok` to continue,
+  /// `ErrCode::ParsingComplete` when finished.
+  [[nodiscard]] ErrCode parseNext() {
+    const CErrCode ret = exip::parseNext(this);
+    return ErrCode(ret);
+  }
+
+  /// @brief Parses all the EXI items.
+  /// @return An `Error` on failure.
+  [[nodiscard]] Error parseAll() {
+    ErrCode lasterr = ErrCode::Ok;
+    while (lasterr == ErrCode::Ok) {
+      lasterr = this->parseNext();
+    }
+    if (lasterr != ErrCode::ParsingComplete)
+      return Error::From(lasterr);
+    return Error::Ok();
+  }
+
+private:
+  [[nodiscard]] Error init(const CBinaryBuffer* buf, void* appData) {
+    auto* const parser = static_cast<CParser*>(this);
+    // CErrCode ret = exip::initParser(parser, *buf, appData);
+    CErrCode ret = exip::parse.initParser(parser, *buf, appData);
+    if (ret != CErrCode::EXIP_OK) {
+      const auto err = ErrCode(ret);
+      return Error::From(err);
+    }
+    return Error::Ok();
+  }
+
+  void deinit() {
+    exip::destroyParser(this);
+    if (CParser::strm.schema) {
+      // exip::destroySchema(CParser::strm.schema);
+    }
+    if (shutdown)
+      shutdown(this);
+  }
+};
+
+//======================================================================//
+// XMLReader
+//======================================================================//
 
 } // namespace exi
 
