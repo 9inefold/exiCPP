5dc6b9238cc0307458b353b234bf4e3a39975d7e
eightfold <97198162+8ightfold@users.noreply.github.com>
2025-01-08 16:53:50 -0500

    Update ConfigureFiles.cmake

--- scripts/ConfigureFiles.cmake
+++ scripts/ConfigureFiles.cmake
@@ -1,33 +1,5 @@
 include_guard(DIRECTORY)
 
-macro(set_parent)
-  foreach(arg ${ARGN})
-    set(${arg} ${${arg}} PARENT_SCOPE)
-  endforeach()
-endmacro()
-
-function(include_items name folder)
-  set(LIST_SRCS ${ARGN})
-  string(REGEX REPLACE "^\/|\/$" "" folder ${folder})
-  set(folder "${CMAKE_CURRENT_SOURCE_DIR}/${folder}")
-  list(TRANSFORM LIST_SRCS PREPEND "${folder}/")
-  set(${name} "${LIST_SRCS}" PARENT_SCOPE)
-endfunction()
-
-macro(list_get ls idx out)
-  list(GET ${ls} ${idx} ${out})
-endmacro(list_get)
-
-function(list_get_or ls idx alt out)
-  list(LENGTH ${ls} _LIST_LEN)
-  if(${idx} LESS _LIST_LEN)
-    list_get(${ls} ${idx} _LIST_OUT)
-  else()
-    set(_LIST_OUT ${alt})
-  endif()
-  set(${out} ${_LIST_OUT} PARENT_SCOPE)
-endfunction(list_get_or)
-
 # Calls `configure_file(...)` on the input name.
 # Must have a `.in` extension, as it will be removed.
 # Newline style is always LF.
