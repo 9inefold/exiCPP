43df3feca4f7652871de1b2f5197fd5d57681973
eightfold <97198162+8ightfold@users.noreply.github.com>
2025-04-10 13:40:10 -0400

    Fix tag lookup, add emplace
    AND FIXED THOSE TABS OML

--- include/core/Common/Poly.hpp
+++ include/core/Common/Poly.hpp
@@ -38,7 +38,7 @@
 namespace exi {
 
 template <class Base,
-	std::derived_from<Base>...Derived>
+  std::derived_from<Base>...Derived>
 class Poly;
 
 namespace poly_detail {
@@ -92,7 +92,7 @@ struct VisitReturn { using type = Ret; };
 
 template <class Obj, class...TT>
 struct VisitReturn<dummy_t, Obj, TT...> {
-	using type = common_ref<std::invoke_result_t<Obj&, TT&>...>;
+  using type = common_ref<std::invoke_result_t<Obj&, TT&>...>;
 };
 
 template <typename Ret, class Obj, class...TT>
@@ -103,13 +103,13 @@ using visit_ret = typename VisitReturn<Ret, Obj, TT...>::type;
 /// Casts and launders a pointer to the requested type.
 template <typename Out, typename In>
 [[nodiscard]] constexpr Out* launder_cast(In* Ptr) noexcept {
-	return std::launder(reinterpret_cast<Out*>(Ptr));
+  return std::launder(reinterpret_cast<Out*>(Ptr));
 }
 
 template <class T, int I>
 struct PolyLeaf {
-	static consteval type_c<T> at(idx_c<I>) { return {}; }
-	static consteval idx_c<I> at(type_c<T>) { return {}; }
+  static consteval type_c<T> at(idx_c<I>) { return {}; }
+  static consteval idx_c<I> at(type_c<T>) { return {}; }
 };
 
 template <class Seq, class...TT>
@@ -117,339 +117,351 @@ struct PolyBranch;
 
 template <usize...II, class...TT>
 struct EXI_EMPTY_BASES
-	PolyBranch<idxseq<II...>, TT...>
-		: public PolyLeaf<TT, II>... {
-	using PolyLeaf<TT, II>::at...;
+  PolyBranch<idxseq<II...>, TT...>
+    : public PolyLeaf<TT, II>... {
+  using PolyLeaf<TT, II>::at...;
 };
 
 template <class Base, class...Derived>
 using poly_branch = PolyBranch<
-	make_idxseq<sizeof...(Derived) + 1>,
-	Base, Derived...
+  make_idxseq<sizeof...(Derived) + 1>,
+  Base, Derived...
 >;
 
 template <class Base, class...Derived>
 class EXI_EMPTY_BASES Storage
-		: public poly_branch<Base, Derived...> {
+    : public poly_branch<Base, Derived...> {
 protected:
-	using poly_branch<Base, Derived...>::at;
-	AlignedCharArrayUnion<Base, Derived...> Data;
-	int Tag = 0;
+  using poly_branch<Base, Derived...>::at;
+  AlignedCharArrayUnion<Base, Derived...> Data;
+  int Tag = 0;
 public:
-	constexpr Storage() : Data() {}
+  constexpr Storage() : Data() {}
 
-	template <typename U>
-	requires is_one_of<U, Base, Derived...> && concrete<U>
-	Storage(std::in_place_type_t<U>, auto&&...Args)
-			: Tag(tagof(type_c<U>{})) {
-		new (Data.buffer) U(EXI_FWD(Args)...);
-	}
+  template <typename U>
+  requires is_one_of<U, Base, Derived...> && concrete<U>
+  Storage(std::in_place_type_t<U>, auto&&...Args)
+      : Tag(tagof(type_c<U>{})) {
+    new (Data.buffer) U(EXI_FWD(Args)...);
+  }
 
-	static constexpr decltype(auto)
-	 From(Poly<Base, Derived...>& Child) noexcept {
-		return static_cast<Storage&>(Child);
-	}
+  static constexpr decltype(auto)
+   From(Poly<Base, Derived...>& Child) noexcept {
+    return static_cast<Storage&>(Child);
+  }
 
-	static constexpr decltype(auto)
-	 From(const Poly<Base, Derived...>& Child) noexcept {
-		return static_cast<const Storage&>(Child);
-	}
+  static constexpr decltype(auto)
+   From(const Poly<Base, Derived...>& Child) noexcept {
+    return static_cast<const Storage&>(Child);
+  }
 
-	template <typename U>
-	requires is_one_of<U, Base, Derived...>
-	static constexpr int tagof(type_c<U>) noexcept {
-		return at(type_c<U>{}) + 1;
-	}
+  template <typename U>
+  requires is_one_of<U, Base, Derived...>
+  static constexpr int tagof(type_c<U>) noexcept {
+    return at(type_c<U>{}) + 1;
+  }
 
-	/// Checks if the object holds the requested type.
-	template <typename U>
-	requires is_one_of<U, Base, Derived...>
-	ALWAYS_INLINE constexpr bool is() const noexcept {
-		constexpr_static int UTag = tagof(type_c<U>{});
-		if constexpr (!abstract<U>)
-			return this->Tag == UTag;
-		else
-			// Abstract types can never be constructed.
-			return false;
-	}
+  /// Checks if the object holds the requested type.
+  template <typename U>
+  requires is_one_of<U, Base, Derived...>
+  ALWAYS_INLINE constexpr bool is() const noexcept {
+    constexpr_static int UTag = tagof(type_c<U>{});
+    if constexpr (!abstract<U>)
+      return this->Tag == UTag;
+    else
+      // Abstract types can never be constructed.
+      return false;
+  }
 
-	constexpr bool empty() const noexcept {
-		return this->Tag == 0;
-	}
+  constexpr bool empty() const noexcept {
+    return this->Tag == 0;
+  }
 
-	/// Gets a pointer to the base object.
-	Base* base() noexcept {
-		return launder_cast<Base>(Data.buffer);
-	}
+  /// Gets a pointer to the base object.
+  Base* base() noexcept {
+    return launder_cast<Base>(Data.buffer);
+  }
 
-	/// Gets a const pointer to the base object.
-	const Base* base() const noexcept {
-		return launder_cast<const Base>(Data.buffer);
-	}
+  /// Gets a const pointer to the base object.
+  const Base* base() const noexcept {
+    return launder_cast<const Base>(Data.buffer);
+  }
 
-	/// Gets a pointer to the object of type `U`.
-	template <typename U>
-	requires is_one_of<U, Base, Derived...>
-	U& as() noexcept {
-		exi_invariant(is<U>());
-		return *launder_cast<U>(Data.buffer);
-	}
+  /// Gets a pointer to the object of type `U`.
+  template <typename U>
+  requires is_one_of<U, Base, Derived...>
+  U& as() noexcept {
+    exi_invariant(is<U>());
+    return *launder_cast<U>(Data.buffer);
+  }
 
-	/// Gets a const pointer to the object of type `U`.
-	template <typename U>
-	requires is_one_of<U, Base, Derived...>
-	const U& as() const noexcept {
-		exi_invariant(is<U>());
-		return *launder_cast<const U>(Data.buffer);
-	}
+  /// Gets a const pointer to the object of type `U`.
+  template <typename U>
+  requires is_one_of<U, Base, Derived...>
+  const U& as() const noexcept {
+    exi_invariant(is<U>());
+    return *launder_cast<const U>(Data.buffer);
+  }
 
-	template <typename U>
-	requires is_one_of<U, Base, Derived...>
-	void emplace(auto&&...Args) {
-		this->Tag = tagof(type_c<U>{});
-		new (Data.buffer) U(EXI_FWD(Args)...);
-	}
+  template <typename U>
+  requires is_one_of<U, Base, Derived...>
+  void emplace(auto&&...Args) {
+    this->Tag = tagof(type_c<U>{});
+    new (Data.buffer) U(EXI_FWD(Args)...);
+  }
 
-	template <typename U>
-	requires is_one_of<U, Base, Derived...>
-	void emplace(std::in_place_type_t<U>, auto&&...Args) {
-		this->Tag = tagof<U>();
-		new (Data.buffer) U(EXI_FWD(Args)...);
-	}
+  template <typename U>
+  requires is_one_of<U, Base, Derived...>
+  void emplace(std::in_place_type_t<U>, auto&&...Args) {
+    this->Tag = tagof(type_c<U>{});
+    new (Data.buffer) U(EXI_FWD(Args)...);
+  }
 };
 
 struct Dtor {
-	template <typename T>
-	constexpr void operator()(T& Val) const noexcept {
-		Val.~T();
-	}
+  template <typename T>
+  constexpr void operator()(T& Val) const noexcept {
+    Val.~T();
+  }
 };
 
 } // namespace poly_detail
 
 template <class Base,
-	std::derived_from<Base>...Derived>
+  std::derived_from<Base>...Derived>
 class Poly final : private poly_detail::Storage<Base, Derived...> {
-	static_assert((... && poly_detail::concrete<Derived>),
-		"All derived types must be concrete!");
+  static_assert((... && poly_detail::concrete<Derived>),
+    "All derived types must be concrete!");
 
-	friend class poly_detail::Storage<Base, Derived...>;
-	using Super = poly_detail::Storage<Base, Derived...>;
+  friend class poly_detail::Storage<Base, Derived...>;
+  using Super = poly_detail::Storage<Base, Derived...>;
 
-	/// Finds the real return type of a visit.
-	template <typename R, typename F>
-	using visit_t = poly_detail::visit_ret<R, F, Base, Derived...>;
+  /// Finds the real return type of a visit.
+  template <typename R, typename F>
+  using visit_t = poly_detail::visit_ret<R, F, Base, Derived...>;
 
-	/// Finds the real return type of a visit.
-	template <typename R, typename F>
-	using cvisit_t = poly_detail::visit_ret<R, F,
-		const Base, std::add_const_t<Derived>...>;
+  /// Finds the real return type of a visit.
+  template <typename R, typename F>
+  using cvisit_t = poly_detail::visit_ret<R, F,
+    const Base, std::add_const_t<Derived>...>;
 
 public:
-	constexpr Poly() : Super() {}
-	constexpr Poly(std::nullopt_t) : Super() {}
+  constexpr Poly() : Super() {}
+  constexpr Poly(std::nullopt_t) : Super() {}
 
-	Poly(const Poly& O)
-	 requires poly_detail::all_copyable<Base, Derived...> : Super() {
-		this->operator=(O);
-	}
+  Poly(const Poly& O)
+   requires poly_detail::all_copyable<Base, Derived...> : Super() {
+    this->operator=(O);
+  }
 
-	Poly(Poly&& O) requires poly_detail::all_movable<Base, Derived...> : Super() {
-		this->operator=(std::move(O));
-	}
+  Poly(Poly&& O) requires poly_detail::all_movable<Base, Derived...> : Super() {
+    this->operator=(std::move(O));
+  }
 
-	template <typename U>
-	Poly(std::in_place_type_t<U>, auto&&...Args) : 
-	 Super(std::in_place_type<U>, EXI_FWD(Args)...) {
-	}
+  template <typename U>
+  Poly(std::in_place_type_t<U>, auto&&...Args) : 
+   Super(std::in_place_type<U>, EXI_FWD(Args)...) {
+  }
 
-	template <is_one_of<Base, Derived...> U>
-	Poly(const U& Value) :
-	 Super(std::in_place_type<U>, Value) {
-	}
+  template <is_one_of<Base, Derived...> U>
+  Poly(const U& Value) :
+   Super(std::in_place_type<U>, Value) {
+  }
 
-	template <is_one_of<Base, Derived...> U>
-	Poly(U&& Value) :
-	 Super(std::in_place_type<U>, std::move(Value)) {
-	}
+  template <is_one_of<Base, Derived...> U>
+  Poly(U&& Value) :
+   Super(std::in_place_type<U>, std::move(Value)) {
+  }
 
-	Poly& operator=(const Poly& O)
-	 requires poly_detail::all_copyable<Base, Derived...> {
-		this->reset();
-		if EXI_LIKELY(!O.empty()) {
-			O.visitSelf<void>([this] <typename T> (const T& Val) {
-				this->Super::template emplace<T>(Val);
-			});
-		}
-		return *this;
-	}
+  Poly& operator=(const Poly& O)
+   requires poly_detail::all_copyable<Base, Derived...> {
+    this->reset();
+    if EXI_LIKELY(!O.empty()) {
+      O.visitSelf<void>([this] <typename T> (const T& Val) {
+        this->Super::template emplace<T>(Val);
+      });
+    }
+    return *this;
+  }
 
-	Poly& operator=(Poly&& O) noexcept
-	 requires poly_detail::all_movable<Base, Derived...> {
-		this->reset();
-		if EXI_LIKELY(!O.empty()) {
-			O.visitSelf<void>([this] <typename T> (T& Val) {
-				this->Super::template emplace<T>(std::move(Val));
-			});
-		}
-		O.reset();
-		return *this;
-	}
+  Poly& operator=(Poly&& O) noexcept
+   requires poly_detail::all_movable<Base, Derived...> {
+    this->reset();
+    if EXI_LIKELY(!O.empty()) {
+      O.visitSelf<void>([this] <typename T> (T& Val) {
+        this->Super::template emplace<T>(std::move(Val));
+      });
+    }
+    O.reset();
+    return *this;
+  }
 
-	template <is_one_of<Base, Derived...> U>
-	Poly& operator=(const U& Value) {
-		this->destroy();
-		Super::template emplace<U>(Value);
-		return *this;
-	}
+  template <is_one_of<Base, Derived...> U>
+  Poly& operator=(const U& Value) {
+    this->destroy();
+    Super::template emplace<U>(Value);
+    return *this;
+  }
 
-	template <is_one_of<Base, Derived...> U>
-	Poly& operator=(U&& Value) {
-		this->destroy();
-		Super::template emplace<U>(std::move(Value));
-		return *this;
-	}
+  template <is_one_of<Base, Derived...> U>
+  Poly& operator=(U&& Value) {
+    this->destroy();
+    Super::template emplace<U>(std::move(Value));
+    return *this;
+  }
 
-	~Poly() { this->destroy(); }
+  ~Poly() { this->destroy(); }
 
-	using Super::empty;
-	constexpr bool has_value() const noexcept { return empty(); }
+  using Super::empty;
+  constexpr bool has_value() const noexcept { return empty(); }
 
-	template <typename U>
-	constexpr bool isa() const noexcept {
-		return Super::template is<U>();
-	}
+  /// Visits the active type with `Func`.
+  template <typename R = dummy_t, class F>
+  auto visit(F&& Func) -> visit_t<R, F>;
 
-	/// Visits the active type with `Func`.
-	template <typename R = dummy_t, class F>
-	auto visit(F&& Func) -> visit_t<R, F>;
+  /// Visits the active type with `Func`.
+  template <typename R = dummy_t, class F>
+  auto visit(F&& Func) const -> cvisit_t<R, F>;
 
-	/// Visits the active type with `Func`.
-	template <typename R = dummy_t, class F>
-	auto visit(F&& Func) const -> cvisit_t<R, F>;
+  /// Constructs a new object of type `U`.
+  template <typename U>
+  requires is_one_of<U, Base, Derived...>
+  void emplace(auto&&...Args) {
+    this->destroy();
+    Super::template emplace<U>(EXI_FWD(Args)...);
+  }
 
-	void reset() {
-		this->destroy();
-		Super::Tag = 0;
-	}
+  /// Constructs a new object of type `U`.
+  template <typename U>
+  requires is_one_of<U, Base, Derived...>
+  void emplace(std::in_place_type_t<U>, auto&&...Args) {
+    this->destroy();
+    Super::template emplace<U>(EXI_FWD(Args)...);
+  }
 
-	Base* operator->() & noexcept
-	 requires poly_detail::polymorphic<Base> {
-		return Super::base();
-	}
+  void reset() {
+    this->destroy();
+    Super::Tag = 0;
+  }
 
-	const Base* operator->() const& noexcept
-	 requires poly_detail::polymorphic<Base> {
-		return Super::base();
-	}
+  Base* operator->() & noexcept
+   requires poly_detail::polymorphic<Base> {
+    return Super::base();
+  }
+
+  const Base* operator->() const& noexcept
+   requires poly_detail::polymorphic<Base> {
+    return Super::base();
+  }
 
 private:
-	void destroy() noexcept {
-		visitSelf<void>(poly_detail::Dtor{});
-	}
+  void destroy() noexcept {
+    if (!empty())
+      visitSelf<void>(poly_detail::Dtor{});
+  }
 
-	template <class U, typename Ret>
-	EXI_NODEBUG ALWAYS_INLINE bool Case(auto& Func) {
-		if (Super::template is<U>()) {
-			if constexpr (poly_detail::concrete<U>)
-				Func(Super::template as<U>());
-			return true;
-		}
-		return false;
-	}
+  template <class U, typename Ret>
+  EXI_NODEBUG ALWAYS_INLINE bool Case(auto& Func) {
+    if (Super::template is<U>()) {
+      if constexpr (poly_detail::concrete<U>)
+        Func(Super::template as<U>());
+      return true;
+    }
+    return false;
+  }
 
-	template <class U, typename Ret>
-	EXI_NODEBUG ALWAYS_INLINE bool Case(auto& Func) const {
-		if (Super::template is<U>()) {
-			if constexpr (poly_detail::concrete<U>)
-				Func(Super::template as<U>());
-			return true;
-		}
-		return false;
-	}
+  template <class U, typename Ret>
+  EXI_NODEBUG ALWAYS_INLINE bool Case(auto& Func) const {
+    if (Super::template is<U>()) {
+      if constexpr (poly_detail::concrete<U>)
+        Func(Super::template as<U>());
+      return true;
+    }
+    return false;
+  }
 
-	template <class U, typename Ret>
-	EXI_NODEBUG ALWAYS_INLINE bool Case(
-	 auto& Func, Option<Ret>& Out) {
-		if (Super::template is<U>()) {
-			if constexpr (poly_detail::concrete<U>)
-				Out.emplace(Func(Super::template as<U>()));
-			return true;
-		}
-		return false;
-	}
+  template <class U, typename Ret>
+  EXI_NODEBUG ALWAYS_INLINE bool Case(
+   auto& Func, Option<Ret>& Out) {
+    if (Super::template is<U>()) {
+      if constexpr (poly_detail::concrete<U>)
+        Out.emplace(Func(Super::template as<U>()));
+      return true;
+    }
+    return false;
+  }
 
-	template <class U, typename Ret>
-	EXI_NODEBUG ALWAYS_INLINE bool Case(
-	 auto& Func, Option<Ret>& Out) const {
-		if (Super::template is<U>()) {
-			if constexpr (poly_detail::concrete<U>)
-				Out.emplace(Func(Super::template as<U>()));
-			return true;
-		}
-		return false;
-	}
+  template <class U, typename Ret>
+  EXI_NODEBUG ALWAYS_INLINE bool Case(
+   auto& Func, Option<Ret>& Out) const {
+    if (Super::template is<U>()) {
+      if constexpr (poly_detail::concrete<U>)
+        Out.emplace(Func(Super::template as<U>()));
+      return true;
+    }
+    return false;
+  }
 
-	template <typename Ret>
-	ALWAYS_INLINE bool Default() const { return true; }
+  template <typename Ret>
+  ALWAYS_INLINE bool Default() const { return true; }
 
-	template <typename Ret>
-	ALWAYS_INLINE bool Default(Option<Ret>& Out) const {
-		if constexpr (std::default_initializable<Ret>) {
-			Out.emplace();
-			return true;
-		}
+  template <typename Ret>
+  ALWAYS_INLINE bool Default(Option<Ret>& Out) const {
+    if constexpr (std::default_initializable<Ret>) {
+      Out.emplace();
+      return true;
+    }
 
-		exi_unreachable("Hit unreachable in Poly::visit!");
-	}
+    exi_unreachable("Hit unreachable in Poly::visit!");
+  }
 
-	template <typename Ret>
-	EXI_INLINE Ret visitSelf(auto&& Func) {
-		if constexpr (std::is_void_v<Ret>) {
-			Case<Base, Ret>(Func)
-				|| (... || Case<Derived, Ret>(Func))
-				|| Default<Ret>();
-		} else {
-			Option<Ret> Out;
-			Case<Base>(Func, Out)
-				|| (... || Case<Derived>(Func, Out))
-				|| Default(Out);
-			return std::move(Out).value();
-		}
-	}
+  template <typename Ret>
+  EXI_INLINE Ret visitSelf(auto&& Func) {
+    if constexpr (std::is_void_v<Ret>) {
+      Case<Base, Ret>(Func)
+        || (... || Case<Derived, Ret>(Func))
+        || Default<Ret>();
+    } else {
+      Option<Ret> Out;
+      Case<Base>(Func, Out)
+        || (... || Case<Derived>(Func, Out))
+        || Default(Out);
+      return std::move(Out).value();
+    }
+  }
 
-	template <typename Ret>
-	EXI_INLINE Ret visitSelf(auto&& Func) const {
-		if constexpr (std::is_void_v<Ret>) {
-			Case<Base, Ret>(Func)
-				|| (... || Case<Derived, Ret>(Func))
-				|| Default<Ret>();
-		} else {
-			Option<Ret> Out;
-			Case<Base>(Func, Out)
-				|| (... || Case<Derived>(Func, Out))
-				|| Default(Out);
-			return std::move(Out).value();
-		}
-	}
+  template <typename Ret>
+  EXI_INLINE Ret visitSelf(auto&& Func) const {
+    if constexpr (std::is_void_v<Ret>) {
+      Case<Base, Ret>(Func)
+        || (... || Case<Derived, Ret>(Func))
+        || Default<Ret>();
+    } else {
+      Option<Ret> Out;
+      Case<Base>(Func, Out)
+        || (... || Case<Derived>(Func, Out))
+        || Default(Out);
+      return std::move(Out).value();
+    }
+  }
 };
 
 template <class Base,
-	std::derived_from<Base>...Derived>
+  std::derived_from<Base>...Derived>
 template <typename R, class F>
 auto Poly<Base, Derived...>::visit(F&& Func)
  -> Poly<Base, Derived...>::template visit_t<R, F> {
-	using Ret = visit_t<R, F>;
-	return visitSelf<Ret>(Func);
+  using Ret = visit_t<R, F>;
+  return visitSelf<Ret>(Func);
 }
 
 template <class Base,
-	std::derived_from<Base>...Derived>
+  std::derived_from<Base>...Derived>
 template <typename R, class F>
 auto Poly<Base, Derived...>::visit(F&& Func) const
  -> Poly<Base, Derived...>::template cvisit_t<R, F> {
-	using Ret = cvisit_t<R, F>;
-	return visitSelf<Ret>(Func);
+  using Ret = cvisit_t<R, F>;
+  return visitSelf<Ret>(Func);
 }
 
 //===----------------------------------------------------------------===//
@@ -463,34 +475,34 @@ struct CastInfo<To, Poly<TT...>>
     : public DefaultDoCastIfPossible<To, Poly<TT...>&,
                             CastInfo<To, Poly<TT...>>> {
   using From = Poly<TT...>;
-	using Super = poly_detail::Storage<TT...>;
+  using Super = poly_detail::Storage<TT...>;
 
   static inline bool isPossible(From& Val) {
-		auto& Base = Super::From(Val);
+    auto& Base = Super::From(Val);
     return Base.template is<To>();
   }
 
   static To& doCast(From& Val) {
-		auto& Base = Super::From(Val);
-		auto& Data = Base.template as<To>();
-		return static_cast<To>(Data);
-	}
+    auto& Base = Super::From(Val);
+    auto& Data = Base.template as<To>();
+    return static_cast<To>(Data);
+  }
 
   static inline auto castFailed() {
-		return std::nullopt;
-	}
+    return std::nullopt;
+  }
 };
 
 template <typename To, typename...TT>
 requires is_one_of<To, TT...>
 struct isa_impl<To, Poly<TT...>> {
-	using From = Poly<TT...>;
-	using Super = poly_detail::Storage<TT...>;
+  using From = Poly<TT...>;
+  using Super = poly_detail::Storage<TT...>;
 
   static inline bool doit(const From &Val) {
-		auto& Base = Super::From(Val);
-		return Base.template is<To>(); 
-	}
+    auto& Base = Super::From(Val);
+    return Base.template is<To>(); 
+  }
 };
 
 //template <typename To, typename...TT>
