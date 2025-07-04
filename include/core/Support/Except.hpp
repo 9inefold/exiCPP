a5e1e3199db1dcdc8805613146512b1b664fec53
eightfold <97198162+9inefold@users.noreply.github.com>
2025-07-04 16:58:07 -0400

    Create Except/Throw<Ex>(...)
    Wraps exception throwing when it is enabled, and fatal reporting when disabled.

---
+++ include/core/Support/Except.hpp
@@ -0,0 +1,55 @@
+//===- Support/Except.hpp -------------------------------------------===//
+//
+// Copyright (C) 2025 Eightfold
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+//     limitations under the License.
+//
+//===----------------------------------------------------------------===//
+///
+/// \file
+/// This file defines handlers for "exceptional" cases.
+///
+//===----------------------------------------------------------------===//
+
+#pragma once
+
+#include <Common/Features.hpp>
+#include <Common/StrRef.hpp>
+#include <Support/ErrorHandle.hpp>
+#include <type_traits>
+#include <stdexcept>
+
+namespace exi {
+namespace except_detail {
+template <typename Ex>
+[[noreturn]] ALWAYS_INLINE EXI_NODEBUG void ThrowImpl(const char* Msg) {
+#if EXI_EXCEPTIONS
+  throw Ex(Msg);
+#else
+  if constexpr (std::is_base_of_v<std::exception, Ex>) {
+    Ex Val(Msg);
+    exi::report_fatal_error(Val.what());
+  } else {
+    exi::report_fatal_error(Msg);
+  }
+#endif
+}
+} // namespace H
+
+/// Implements "throwing" in a way that reduces register pressure.
+template <typename Ex>
+[[noreturn]] EXI_ERROR_CC void Throw(StringLiteral Msg = "") {
+  except_detail::ThrowImpl<Ex>(Msg.data());
+}
+
+} // namespace exi
