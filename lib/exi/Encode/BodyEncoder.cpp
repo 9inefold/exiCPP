afd7e1fcbbdadcdabd3862978431530d65832af9
eightfold <97198162+9inefold@users.noreply.github.com>
2025-07-11 17:08:21 -0400

    BodyEncoder
    This will also be the general look of the ExiDecoder once updated

---
+++ lib/exi/Encode/BodyEncoder.cpp
@@ -0,0 +1,53 @@
+//===- exi/Encode/BodyEncoder.cpp -----------------------------------===//
+//
+// Copyright (C) 2025 Eightfold
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+//     limitations under the License.
+//
+//===----------------------------------------------------------------===//
+///
+/// \file
+/// This file provides the interface for encoding of the EXI body to a stream.
+///
+//===----------------------------------------------------------------===//
+
+#include <exi/Encode/BodyEncoder.hpp>
+#include <core/Common/MMatch.hpp>
+#include <core/Support/Casting.hpp>
+#include <core/Support/Format.hpp>
+#include <core/Support/Logging.hpp>
+#include <core/Support/raw_ostream.hpp>
+#include <exi/Basic/ErrorCodes.hpp>
+
+#define DEBUG_TYPE "ExiEncoder"
+
+using namespace exi;
+using namespace exi::encode;
+
+ExiEncoder::ExiEncoder(MaybeBox<ExiOptions> Opts,
+                       Option<raw_ostream&> OS) : ExiEncoder(OS) {
+  Header.Opts = std::move(Opts);
+}
+
+ExiEncoder::~ExiEncoder() {
+  os().flush()
+}
+
+//////////////////////////////////////////////////////////////////////////
+// Miscellaneous
+
+void BodyEncoder::anchor() {}
+
+raw_ostream& ExiEncoder::os() const {
+  return OS.value_or(errs());
+}
---
+++ lib/exi/Encode/OrderedEncoder.cpp
@@ -0,0 +1,41 @@
+//===- exi/Encode/OrderedEncoder.cpp --------------------------------===//
+//
+// Copyright (C) 2025 Eightfold
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+//     limitations under the License.
+//
+//===----------------------------------------------------------------===//
+///
+/// \file
+/// This file implements an exi processor for in-order writers.
+///
+//===----------------------------------------------------------------===//
+
+#include <exi/Encode/OrderedEncoder.hpp>
+#include <core/Common/MMatch.hpp>
+#include <core/Common/Unwrap.hpp>
+#include <core/Support/Casting.hpp>
+#include <core/Support/Format.hpp>
+#include <core/Support/Logging.hpp>
+#include <core/Support/raw_ostream.hpp>
+#include <exi/Basic/ErrorCodes.hpp>
+#include <exi/Basic/Runes.hpp>
+
+#define DEBUG_TYPE "OrderedEncoder"
+
+using namespace exi;
+using namespace exi::encode;
+
+ExiError OrderedEncoder::run() {
+
+}
