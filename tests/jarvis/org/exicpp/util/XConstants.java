03dbc9310fc7955e95dbc1def7e06b3dabe136c5
eightfold <97198162+9inefold@users.noreply.github.com>
2026-01-30 21:55:07 -0500

    Add XConstants

---
+++ tests/jarvis/org/exicpp/util/XConstants.java
@@ -0,0 +1,27 @@
+//===- util/Constants.java ------------------------------------------===//
+//
+// Copyright (C) 2026 Ninefold
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+//     limitations under the License.
+//
+//===----------------------------------------------------------------===//
+
+package org.exicpp.util;
+
+public class XConstants {
+  /** exicpp features prefix ("http://apache.org/xml/features/"). */
+  public static final String EXICPP_FEATURE_PREFIX = "http://exicpp.org/xml/features/";
+
+  /** Embed escape sequences */
+  public static final String EMBED_ESCAPE_SEQUENCES = "scanner/embed-escape-sequences";
+}
