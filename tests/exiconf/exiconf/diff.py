16dc01c196e25f63d66c4ea57d47647e7908aea1
eightfold <97198162+9inefold@users.noreply.github.com>
2026-03-06 22:54:35 -0500

    Document diff params

--- tests/exiconf/exiconf/diff.py
+++ tests/exiconf/exiconf/diff.py
@@ -12,6 +12,44 @@ __all__ = [
   'diff_files',
 ]
 
+"""
+parse_options boolean arguments:
+
+  attribute_defaults - inject default attributes from DTD or XMLSchema
+  dtd_validation - validate against a DTD referenced by the document
+  load_dtd - use DTD for parsing
+  no_network - prevent network access for related files (default: True)
+  ns_clean - clean up redundant namespace declarations
+  recover - try hard to parse through broken XML
+  remove_blank_text - discard blank text nodes that appear ignorable
+  remove_comments - discard comments
+  remove_pis - discard processing instructions
+  strip_cdata - replace CDATA sections by normal text content (default: True)
+  compact - save memory for short text content (default: True)
+  collect_ids - use a hash table of XML IDs for fast access (default: True, always True with DTD validation)
+  resolve_entities - replace entities by their text value (default: True)
+
+  huge_tree - disable security restrictions and support very deep trees
+      and very long text content (only affects libxml2 2.7+)
+
+Other parse_options arguments:
+
+  encoding - override the document encoding
+  target - a parser target object that will receive the parse events
+  schema - an XMLSchema to validate against
+"""
+
+"""
+diff_options arguments:
+
+  F - similarity (default: 0.5)
+  uniqueattrs - list of attributes or (tag, attribute) pairs that uniquely identifies a node inside a document. (default: 'xml:id')
+  ratio_mode - 'fast', 'faster', or 'accurate' (default: 'fast')
+  fast_match - (default: False)
+  best_match - (default: False)
+  ignored_attrs - (default: [])
+"""
+
 def _or(value, other):
   return value if value is not None else other
 
@@ -34,11 +72,9 @@ def _diff_common(name, diff_list, logger) -> bool:
   return True
 
 def _diff_xml(diff_func, name, arg1, arg2, logger, **kwargs) -> bool:
-  if logger is None:
-    logger = errs()
   try:
     diff_list = diff_func(arg1, arg2, **kwargs)
-    return _diff_common(name, diff_list, logger)
+    return _diff_common(name, diff_list, _or(logger, errs()))
   except Exception as e:
     logger.error(f'[diff] {Path(name).as_posix()}*: {type(e).__name__}: {e}')
     return False
